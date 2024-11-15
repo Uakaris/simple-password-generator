f0a1f01843cf1a981492f26c0e5a65bf48e0293c
Charlie Jay <charliejbjay@gmail.com>
2024-11-15 11:40:48 +1100

    Implemented score method for calculating password strength

--- src/Components/strengthCheck.jsx
+++ src/Components/strengthCheck.jsx
@@ -2,15 +2,28 @@ import React from "react";
 
 const PasswordStrengthIndicator = ({ password = "" }) => {
     const getPasswordStrength = () => {
-        const passwordLength = password.length;
+        const length = password.length;
+        const hasLowerCase = /[a-z]/.test(password);
+        const hasUpperCase = /[A-Z]/.test(password);
+        const hasNumbers = /\d/.test(password);
+        const hasSpecialChars = /[!@#$%^&*()]/.test(password);
 
-        if (passwordLength < 1) {
-            return "";
-        } else if (passwordLength < 4) {
+        if (length === 0) return "";
+
+        let score = 0;
+
+        if (hasLowerCase) score++;
+        if (hasUpperCase) score++;
+        if (hasNumbers) score++;
+        if (hasSpecialChars) score++;
+        if (length >= 8) score++;
+        if (length >= 12) score++;
+
+        if (score <= 2) {
             return "Weak";
-        } else if (passwordLength < 8) {
+        } else if (score === 3) {
             return "Moderate";
-        } else if (passwordLength < 12) {
+        } else if (score === 4) {
             return "Strong";
         } else {
             return "Very Strong";
